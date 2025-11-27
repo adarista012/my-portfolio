948ed80e49f7bb9f19c24a68ea7cee0d43c48ed1
adarista012 <ad.arista012@gmail.com>
2025-11-27 14:01:49 -0400

    contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,12 +2,15 @@ export default function Contact() {
   return (
     <section className='py-20 text-center'>
       <h2 className='text-3xl font-bold'>Contacto</h2>
-      <p className='mt-4 text-gray-400'>antonio@email.com</p>
+      <p className='mt-4 text-gray-400'>ad.arista012@gmail.com</p>
       <div className='mt-6 flex justify-center gap-6'>
-        <a href='https://github.com/' target='_blank'>
+        <a href='https://github.com/adarista012' target='_blank'>
           GitHub
         </a>
-        <a href='https://linkedin.com/' target='_blank'>
+        <a
+          href='https://www.linkedin.com/in/antonio-dante-arista-rivas-mobile-developer/'
+          target='_blank'
+        >
           LinkedIn
         </a>
       </div>
